7cd9f592244f4bcf7360a4268da91e17731bbe05
sam-klok <sklok11@gmail.com>
2021-12-11 15:03:37 -0700

    Initial commit

---
+++ CreateMaxAmountOfTeams/Program.cs
@@ -0,0 +1,124 @@
+﻿
+
+var skill = new List<int> { 3, 4, 3, 1, 6, 5 };
+int teamSize = 3;
+int maxDiff = 2;
+// result = 2
+Console.WriteLine(countMaximumTeams(skill, teamSize, maxDiff));
+
+skill = new List<int> { 9, 3, 5, 7, 1 };
+teamSize = 2;
+maxDiff = 1;
+// result 0
+Console.WriteLine(countMaximumTeams(skill, teamSize, maxDiff));
+
+skill = new List<int> { 3, 4, 3, 1, 6 };
+teamSize = 3;
+maxDiff = 2;
+// result = 2
+Console.WriteLine(countMaximumTeams(skill, teamSize, maxDiff));
+
+Console.WriteLine("press any key..");
+Console.ReadKey();
+// See https://aka.ms/new-console-template for more information
+
+
+static int countMaximumTeams(List<int> skill, int teamSize, int maxDiff)
+{
+    skill.Sort();
+    skill.Reverse();
+
+    int maxTeamCount = 0;
+
+    for (int k = 0; k < skill.Count; k++)
+    {
+        if (skill.Count < teamSize)
+            break;
+
+        int t0 = 0;  // first in new team
+        int tCount = 0;
+
+
+        for (int j = 0; j < teamSize; j++)
+        {
+            if (tCount >= teamSize)
+                break;
+
+            for (int i = skill.Count; i-- > 0;)
+            {
+                if (tCount == 0)
+                {
+                    t0 = skill[i];
+                    tCount++;
+                    skill.RemoveAt(i);
+                }
+                else
+                {
+                    if (tCount >= teamSize)
+                        break;
+
+                    bool isSkillInRange = skill[i] - t0 <= maxDiff;
+                    if (isSkillInRange)
+                    {
+                        tCount++;
+                        skill.RemoveAt(i);
+                        if (tCount >= teamSize)
+                        {
+                            maxTeamCount++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    return maxTeamCount;
+}
+
+static int countMaximumTeamsSlow(List<int> skill, int teamSize, int maxDiff)
+{
+    skill.Sort();
+    skill.Reverse();
+
+    int maxTeamCount = 0;
+
+    for (int k = 0; k < skill.Count; k++)
+    {
+        var t = new List<int>(); // new team
+        for (int j = 0; j < teamSize; j++)
+        {
+            for (int i = skill.Count; i-- > 0;)
+            {
+                if (t.Count == 0)
+                {
+                    t.Add(skill[i]);
+                    skill.RemoveAt(i);
+                }
+                else
+                {
+                    if (t.Count >= teamSize)
+                        break;
+
+                    bool isSkillInRange = skill[i] - t[0] <= maxDiff;
+                    if (isSkillInRange)
+                    {
+                        t.Add(skill[i]);
+                        skill.RemoveAt(i);
+                        if (t.Count >= teamSize)
+                        {
+                            if (t.Count == teamSize)
+                                maxTeamCount++;
+                            else
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    return maxTeamCount;
+}
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+This is coding exersise. 
+See attached images for explanation of the task.
+
+It's not final, matter of fact, some test on Hacker Rank don't run.
+Definetly needs to be optimized.
+
+thank you
+Sam Klok
